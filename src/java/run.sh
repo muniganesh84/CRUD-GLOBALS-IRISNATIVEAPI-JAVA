7c8797d83dac7486f12c407e5a4a1b43d977036d
r-cemper <146277387+r-cemper@users.noreply.github.com>
2023-11-17 22:01:07 +0100

    jdbc versions change to fast

--- src/java/run.sh
+++ src/java/run.sh
@@ -2,7 +2,7 @@
 
 echo "Building..."
 # javac -classpath /usr/irissys/dev/java/lib/JDK18/intersystems-jdbc-3.1.0.jar -d bin src/IRISNative/IRISNative.java
-javac -classpath /usr/irissys/dev/java/lib/1.8/intersystems-jdbc-3.7.1.jar -d bin src/IRISNative/IRISNative.java
+javac -classpath /usr/irissys/dev/java/lib/1.8/intersystems-jdbc-3.*.jar -d bin src/IRISNative/IRISNative.java
 
 echo "Executing..."
 cd bin
